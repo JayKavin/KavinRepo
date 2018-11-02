9373b323e88bf3cb8517cdd11fe7782de073df5e
JayKavin <44406115+JayKavin@users.noreply.github.com>
2018-11-02 00:26:51 -0400

    merged

--- MavenPrimero/src/main/java/com/primero/qa/config/Dummyclass.java
+++ MavenPrimero/src/main/java/com/primero/qa/config/Dummyclass.java
@@ -8,12 +8,12 @@ public class Dummyclass {
 	//Added from feature branch
 	
 	//Add one more line to feature
-<<<<<<< HEAD
+
 //Added fresh line by Feature branch 1
-=======
+
 	
 	//Changed in Branch 2
 
->>>>>>> refs/remotes/origin/Branch2
+
 
 }
