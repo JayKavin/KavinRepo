56415ab831f9d12ccb048ce78223514649ff0bb9
JayKavin <44406115+JayKavin@users.noreply.github.com>
2018-11-02 15:10:38 -0400

    commited

--- MavenPrimero/src/main/java/com/primero/qa/config/Dummyclass.java
+++ MavenPrimero/src/main/java/com/primero/qa/config/Dummyclass.java
@@ -2,20 +2,6 @@ package com.primero.qa.config;
 
 public class Dummyclass {
 
-	//Added some code in featur branch.
-
-	
-	//Added from feature branch
-	
-	//Add one more line to feature
-
-//Added fresh line by Feature branch 1
-
-	
-	//Changed in Branch 2
-
-
-//Branch 2 change
-
+//This is local master branch
 
 }
