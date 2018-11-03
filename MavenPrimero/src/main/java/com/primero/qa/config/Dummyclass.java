aa3a4de8baedaf5b5e8562f0220e3e15a6f6556c
JayKavin <44406115+JayKavin@users.noreply.github.com>
2018-11-02 22:20:24 -0400

    jaybranch commit1

--- MavenPrimero/src/main/java/com/primero/qa/config/Dummyclass.java
+++ MavenPrimero/src/main/java/com/primero/qa/config/Dummyclass.java
@@ -2,11 +2,6 @@ package com.primero.qa.config;
 
 public class Dummyclass {
 
-//This is local master branch
-
-//Adding to my Jay Branch
-	//jay branch commit
-
-	// local branch eclipse 2
+//Jay branch commit
 
 }
